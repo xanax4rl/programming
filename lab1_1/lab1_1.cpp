7798d05d5cc7a72f51b8a08d5733da6856bdfb68
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-07 21:11:30 +0200

    lab1_1

---
+++ lab1_1/lab1_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+int x = 2, y = 4, z = 6, s1, s2, s3;
+float s;
+
+int main()
+{
+
+	s1 = x * y;
+	s2 = x * z;
+	s3 = y * z;
+
+	s = sqrt(s1 + s2 + s3);
+
+	printf("%f", s);
+	return 0;
+}
