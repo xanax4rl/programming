e1e4623e492eb120519350b0db8b08dae6b43de1
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-07 21:21:21 +0200

    lab1_2

---
+++ lab1_2/lab1_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+
+int main() {
+
+	setlocale(LC_ALL, "Ukrainian");
+
+	double x;
+
+	std::cout << "Введiть значення(дюйми): ";
+
+	std::cin >> x;
+
+
+	std::cout << x << "дюйм у см = " << x * 2.54 << std::endl;
+
+	std::cout << x << "дюйм у мм = " << (x * 2.54) * 10 << std::endl;
+
+	std::cout << x << "дюйм у м = " << (x * 2.54) / 100 << std::endl;
+
+	return 0;
+
+}
