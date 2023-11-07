f813a6b6b913a2fb63bddbee8e80e6aed839417a
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-07 22:04:15 +0200

    lab3_2

---
+++ lab3_2/lab3_2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+
+
+    setlocale(LC_ALL, "Ukrainian");
+
+
+
+    double x;
+
+
+
+    cout << "Введіть x: ";
+
+    cin >> x;
+
+
+
+    cout << "Відповідь: " << sqrt(pow(x, 3) - sqrt(x - 1));
+
+
+
+
+
+    return 0;
+
+}
