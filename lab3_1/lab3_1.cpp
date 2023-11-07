f6555f5f1a047f0e72ece8491719ca23de6d5b1a
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-07 21:57:31 +0200

    lab3_1

---
+++ lab3_1/lab3_1.cpp
@@ -0,0 +1,67 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+
+
+    setlocale(LC_ALL, "Ukrainian");
+
+
+
+    double n, s = 100, p;
+
+
+
+    cout << "Введіть сумарний бал учня(0-100): ";
+
+    cin >> n;
+
+
+
+    p = (n / s) * 100;
+
+
+
+    if (p >= 92)
+
+    {
+
+        cout << "Ваша оцінка: 12 " << endl;
+
+    }
+
+    else if (p < 92 && p >= 70)
+
+    {
+
+        cout << "Ваша оцінка: 8 " << endl;
+
+    }
+
+    else if (p < 70 && p >= 50)
+
+    {
+
+        cout << "Ваша оцінка: 5 " << endl;
+
+    }
+
+    else if (p < 50)
+
+    {
+
+        cout << "Ваша оцінка: 2 " << endl;
+
+    }
+
+    return 0;
+
+}
