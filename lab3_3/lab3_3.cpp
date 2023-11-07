f9cf64fbd45880a4eb8adba7a7af6866ce2d71c8
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-07 22:10:21 +0200

    lab3_3

---
+++ lab3_3/lab3_3.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+
+int main()
+
+{
+    setlocale(LC_ALL, "Ukrainian");
+    double x, y;
+
+
+
+    cout << "Введіть x:";
+
+    cin >> x;
+
+    cout << "Введіть y:";
+
+    cin >> y;
+
+
+
+    if (x * x + y * y <= 4) {
+
+        if (!(x <= 1 && x >= -1 && y <= 1 && y >= -1))
+
+        {
+
+            cout << "Входить";
+
+        }
+
+    }
+
+    else
+
+    {
+
+        cout << "Не входить";
+
+    }
+
+
+
+    return 0;
+
+}
+
