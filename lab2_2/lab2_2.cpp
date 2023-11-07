cd3a600eec680587ac54b8829a1e1285081030f9
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-07 21:50:04 +0200

    lab2_2

---
+++ lab2_2/lab2_2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+#include <math.h>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+
+
+    setlocale(LC_ALL, "Ukrainian");
+
+
+
+    int z1, z2, A;
+
+    cout << "Маємо 2 формули:" << endl;
+
+    cout << "1. z1 = (sin(2*A)+sin(5*A)-sin(3*A))/((cos(A)+1)- 2*pow(sin(2*A),2))" << endl;
+
+    cout << "2. z2 = 2*sin(A)" << endl;
+
+    cout << "Введіть значення А: ";
+
+    cin >> A;
+
+
+
+    z1 = (sin(2 * A) + sin(5 * A) - sin(3 * A)) / ((cos(A) + 1) - 2 * pow(sin(2 * A), 2));
+
+
+
+    z2 = 2 * sin(A);
+
+
+
+    cout << "z1 = " << z1 << endl;
+
+
+
+    cout << "z2 = " << z2;
+
+}
+
