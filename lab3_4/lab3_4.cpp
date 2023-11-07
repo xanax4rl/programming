9d15ca4423bfcc0983db263434948429cad79720
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-07 22:15:40 +0200

    lab3_4

---
+++ lab3_4/lab3_4.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+
+#include <math.h>
+
+using namespace std;
+
+
+
+int main()
+
+{
+
+
+
+    setlocale(LC_ALL, "Ukrainian");
+
+
+
+    double f, x, c, b, a;
+
+    cout << "Введіть x: ";
+
+    cin >> x;
+
+    cout << "Введіть a: ";
+
+    cin >> a;
+
+    cout << "Введіть b: ";
+
+    cin >> b;
+
+    cout << "Введіть c: ";
+
+    cin >> c;
+
+
+
+    if (x < 5 && c == 0)
+
+    {
+
+        f = (1 / (a * x)) - b;
+
+        cout << "F= " << f << endl;
+
+    }
+
+    else if (x > (-5) && c != 0)
+
+    {
+
+        f = (x - a) / x;
+
+        cout << "F= " << f << endl;
+
+    }
+
+    else
+
+    {
+
+        f = (10 * x) / (c - 4);
+
+        cout << "F= " << f << endl;
+
+    }
+
+
+
+    return 0;
+
+}
