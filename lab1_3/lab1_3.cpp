5403c2dbb96c88d50babc6b6cade9be377e75a7f
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-07 21:29:13 +0200

    lab1_3

---
+++ lab1_3/lab1_3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    double r1, r2, s, p = 3.14;
+    setlocale(LC_ALL, "Ukrainian");
+
+    std::cout << "Введiть R1:";
+    std::cin >> r1;
+
+    std::cout << "Введiть R2(R2>R1):";
+    std::cin >> r2;
+
+    if (r1 < r2) {
+        std::cout << "S кiльця = " << p * ((r2 * r2) - (r1 * r1)) << std::endl;
+    }
+    else {
+        std::cout << "Помилка";
+    }
+    return 0;
+}
