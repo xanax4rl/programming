15b136298c3128b756f7261e9195d82d9a2800b9
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 21:22:42 +0200

    lab6_4

---
+++ lab6_4/lab6_4.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+    
+    int n;
+
+    cout << "Введіть розмір квадратної матриці(вибирайте числа від 2 до 5, вам краще буде): " << endl;
+
+    cin >> n;
+
+    
+    vector<vector<int>> matrix(n, vector<int>(n));
+
+
+    cout << "Введіть " << n * n << " цілих чисел для заповнення масиву:" << endl;
+
+    for (int i = 0; i < n; ++i) {
+
+        for (int j = 0; j < n; ++j) {
+
+            cin >> matrix[i][j];
+
+        }
+
+    }
+
+
+
+    int sum = 0;
+
+    for (int i = 0; i < n; ++i) {
+
+        if (matrix[i][i] > 20) {
+
+            sum += matrix[i][i];
+
+        }
+
+    }
+
+
+
+    cout << "Сума елементів головної діагоналі, більших за 20: " << sum << endl;
+
+
+
+    return 0;
+
+}
