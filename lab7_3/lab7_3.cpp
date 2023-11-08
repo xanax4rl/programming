282bd7ecae2ab9d51d4be07e1410ac596db1d27f
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-09 00:37:50 +0200

    lab7_3

---
+++ lab7_3/lab7_3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    string input;
+    cout << "Введіть рядок: ";
+    cin >> input;
+
+    int sum = 0;  
+    int sign = 1; 
+
+    for (char c : input) {
+        if (isdigit(c)) {
+            int digit = c - '0'; 
+            sum += sign * digit;
+            sign = -sign; 
+        }
+    }
+
+    cout << "Сума цифр зі зміною знаку: " << sum << endl;
+
+    return 0;
+}
