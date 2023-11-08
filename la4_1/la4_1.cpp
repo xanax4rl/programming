cfc7430a5f5e382adddd9481475294377e414048
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 15:43:55 +0200

    lab4_1

---
+++ la4_1/la4_1.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    double s = 1.0;
+
+
+
+    for (double x = 0.1; x <= 10.0; x += 0.1) {
+
+        double a = 100.0 + 10.0 * cos(x);
+
+        s *= a;
+
+    }
+
+
+
+    cout << "Результат: " << s << endl;
+
+
+
+    return 0;
+
+}
