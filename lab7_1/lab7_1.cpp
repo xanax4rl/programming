947e42fabe5ad235f327515200420d9e6116c7af
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 22:33:35 +0200

    lab7_1

---
+++ lab7_1/lab7_1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool isPalindrome(string word) {
+    int left = 0;
+    int right = word.length() - 1;
+
+    while (left < right) {
+        if (word[left] != word[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+    string word;
+
+    cout << "Введіть слово: ";
+    cin >> word;
+
+    if (isPalindrome(word)) {
+        cout << "Це слово є паліндромом" << endl;
+    }
+    else {
+        cout << "Це слово не є паліндромом" << endl;
+    }
+
+    return 0;
+}
