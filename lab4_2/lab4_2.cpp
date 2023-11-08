04eadf45545a47dc0ea41014928d9ed39a77a398
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 15:53:16 +0200

    lab4_2

---
+++ lab4_2/lab4_2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+
+int main() {
+
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    int n;
+
+    cout << "Введіть натуральне число n: ";
+
+    cin >> n;
+
+
+
+    double b = 0.0;
+
+
+
+    for (int i = 1; i <= n; i++) {
+
+        b += 1.0 / i;
+
+    }
+
+
+
+    cout << "Послідовність b[i]: ";
+
+    for (int i = 1; i <= n; i++) {
+
+        b -= 1.0 / i;
+
+        cout << b << " | ";
+
+    }
+
+
+
+    cout << endl;
+
+
+
+    return 0;
+
+}
