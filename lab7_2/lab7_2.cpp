7654857fb575f9dbc806a47fe093717caf50b177
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-09 00:29:13 +0200

    lab7_2

---
+++ lab7_2/lab7_2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+
+
+    string sentence;
+
+    cout << "Введіть речення(на англійській): ";
+    getline(cin, sentence);
+
+    istringstream iss(sentence);
+    vector<string> words;
+
+    string word;
+    while (iss >> word) {
+        words.push_back(word);
+    }
+
+    if (words.size() < 2) {
+        cout << "Речення повинно містити принаймні два слова." << endl;
+        return 1;
+    }
+
+    swap(words[0], words[words.size() - 1]);
+
+    cout << "Результат: ";
+    for (const string& w : words) {
+        cout << w << ' ';
+    }
+    cout << endl;
+
+    return 0;
+}
