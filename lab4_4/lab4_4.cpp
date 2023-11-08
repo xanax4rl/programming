32bf37092d8fc9888280e3ea6c837a04a77672fe
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 16:03:53 +0200

    lab4_4

---
+++ lab4_4/lab4_4.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    int n;
+
+
+
+    cout << "Введіть натуральне n (n > 1): ";
+
+    cin >> n;
+
+
+
+    if (n <= 1) {
+
+        cout << "n повинно бути більше 1." << endl;
+
+        return 1;
+
+    }
+
+
+
+
+
+    int u0 = 0;
+
+    int u1 = 1;
+
+
+
+    cout << "Члени послідовності Фібоначчі:" << endl;
+
+    cout << u0 << " " << u1 << " ";
+
+
+
+    for (int i = 2; i < n; i++) {
+
+        int next = u0 + u1;
+
+        cout << next << " ";
+
+        u0 = u1;
+
+        u1 = next;
+
+    }
+
+
+
+
+
+    return 0;
+
+}
