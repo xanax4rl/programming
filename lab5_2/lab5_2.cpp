d5832af1d2855ee5eb7fa547fd9da9e33b7c393f
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 20:10:03 +0200

    lab5_2

---
+++ lab5_2/lab5_2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    double x;
+
+    double* show_x;
+
+
+    cout << "Введіть х: ";
+
+    cin >> x;
+
+
+    show_x = &x;
+
+
+
+    cout << "Корінь квадратний: " << sqrt(sin(*show_x) + cos(*show_x)) << endl;
+
+
+
+    return 0;
+
+}
