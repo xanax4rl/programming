225ab104c0b473dbb594b018f83318c3c3851554
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 20:31:47 +0200

    lab6_1

---
+++ lab6_1/lab6_1.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+
+int main() {
+
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    const int arraySize = 30;
+
+    double a[arraySize];
+
+    double b[arraySize];
+
+
+
+
+
+    for (int i = 1; i <= arraySize; ++i) {
+
+        a[i] = static_cast<double>(rand()) / RAND_MAX;
+
+        b[i] = static_cast<double>(rand()) / RAND_MAX;
+
+    }
+
+
+
+
+
+    double numerator = 0;
+
+    for (int i = 1; i < arraySize; i += 2) {
+
+        numerator += a[i] * b[i];
+
+    }
+
+
+
+
+
+    double denominator = 0;
+
+    for (int i = 2; i < arraySize; i += 2) {
+
+        denominator += a[i] * b[i];
+
+    }
+
+
+
+
+
+    if (denominator != 0) {
+
+        double result = numerator / denominator;
+
+        cout << "Результат: " << result << endl;
+
+    }
+    else {
+
+        cout << "Знаменник дорівнює нулю, ділення на нуль неможливе." << endl;
+
+    }
+
+
+
+    return 0;
+
+}
