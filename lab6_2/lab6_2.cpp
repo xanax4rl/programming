54eff72cea58b8701e2789bfcf510202cd00e556
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 20:41:54 +0200

    lab6_2

---
+++ lab6_2/lab6_2.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+
+
+    int Massiv[99];
+
+
+
+    for (int i = 0; i <= 100; ++i) {
+
+        Massiv[i] = rand() % 90 + 10;
+
+    }
+
+    cout << "Числа у нормальному порядку: " << Massiv << endl;
+
+    for (int i = 0; i < 100; ++i) {
+
+        cout << Massiv[i] << " ";
+
+        if (i % 6 == 5) {
+
+            cout << endl;
+
+        }
+
+    }
+
+
+
+    cout << endl;
+
+
+
+    cout << "Числа у зворотному порядку по 6 в рядку:" << endl;
+
+    for (int i = 100 - 1; i >= 0; --i) {
+
+        cout << Massiv[i] << " ";
+
+        if (i % 6 == 4) {
+
+            cout << endl;
+
+        }
+
+    }
+
+
+
+    return 0;
+
+}
