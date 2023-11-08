eddddf4e9e66effef42c4dd61d13bee41dbc94e5
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 20:20:19 +0200

    lab5_3

---
+++ lab5_3/lab5_3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    bool* show1 = new bool;
+
+    bool* show2 = new bool;
+
+
+
+    *show1 = true;
+
+    *show2 = false;
+
+
+
+    cout << "Початкові значення:" << endl;
+
+    cout << "*show1 = " << *show1 << endl;
+
+    cout << "*show2 = " << *show2 << endl;
+
+
+
+    bool temp = *show1;
+
+    *show1 = *show2;
+
+    *show2 = temp;
+
+
+
+    cout << "Змінені значення:" << endl;
+
+    cout << "*show1 = " << *show1 << endl;
+
+    cout << "*show2 = " << *show2 << endl;
+
+    return 0;
+}
