da6d7fd15c9a0c71331c5029ec35c9a9b04c110a
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 16:08:39 +0200

    lab4_5

---
+++ lab4_5/lab4_5.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <cmath>
+
+
+
+using namespace std;
+
+
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+    int n;
+
+    double x;
+
+
+
+    cout << "Введіть натуральне число n: ";
+
+    cin >> n;
+
+
+
+    cout << "Введіть дійсне число x: ";
+
+    cin >> x;
+
+
+
+    double sum = 0.0;
+
+
+
+    for (int k = 1; k <= n; ++k) {
+
+        double term = (2.0 * tgamma(k + 1));
+
+        sum += term;
+
+    }
+
+
+
+    cout << "Результат обчислення суми: " << sum << endl;
+
+
+
+    return 0;
+
+}
+
