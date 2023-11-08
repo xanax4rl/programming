1fdf60d632c72e1492c0c7600f664d3c8f15bfd2
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 19:38:01 +0200

    lab5_1

---
+++ lab5_1/lab5_1.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    double a, b;
+
+    double* show_a, * show_b;
+
+
+
+    cout << "Введіть a: ";
+
+    cin >> a;
+
+    cout << "Введіть b: ";
+
+    cin >> b;
+
+
+
+    show_a = &a;
+
+    show_b = &b;
+
+
+
+    if (*show_a > *show_b) {
+
+        *show_a += 7;
+
+        *show_b -= 3;
+
+    }
+    else {
+
+        *show_b += 7;
+
+        *show_a -= 3;
+
+    }
+
+
+
+
+
+    cout << "Змінна a після обробки: " << a << endl;
+
+    cout << "Змінна b після обробки: " << b << endl;
+
+
+
+    return 0;
+
+}
