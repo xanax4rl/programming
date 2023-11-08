293c22ae743392c090d864e6fc9586b551776052
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 22:17:55 +0200

    lab6_6

---
+++ lab6_6/lab6_6.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    int p; 
+    int t; 
+
+    cout << "Введіть кількість рядків: ";
+    cin >> p;
+
+    cout << "Введіть кількість стовпців: ";
+    cin >> t;
+
+    vector<vector<int>> A(p, vector<int>(t));
+
+    for (int i = 0; i < p; i++) {
+        for (int j = 0; j < t; j++) {
+            cout << "A[" << i + 1 << "][" << j + 1 << "]: ";
+            cin >> A[i][j];
+        }
+    }
+
+    vector<int> temp = A[0];
+    A[0] = A[p - 1];
+    A[p - 1] = temp;
+
+    cout << "Змінена таблиця A:" << endl;
+    for (int i = 0; i < p; i++) {
+        for (int j = 0; j < t; j++) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
