d49ee62eda9a88b2e7ce55b8b709bfdab2d7ee63
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 20:51:01 +0200

    lab6_3

---
+++ lab6_3/lab6_3.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    int p, t;
+    
+    
+    cout << "Введіть кількість елементів в послідовності a (p): ";
+    cin >> p;
+
+    cout << "Введіть кількість елементів в послідовності b (t): ";
+    cin >> t;
+
+    vector<double> a(p);
+    vector<double> b(t);
+
+
+    cout << "Введіть елементи послідовності a:" << endl;
+    for (int i = 0; i < p; i++) {
+        cin >> a[i];
+    }
+
+    cout << "Введіть елементи послідовності b:" << endl;
+    for (int i = 0; i < t; i++) {
+        cin >> b[i];
+    }
+
+    set<double> commonElements;
+
+    for (int i = 0; i < p; i++) {
+        for (int j = 0; j < t; j++) {
+            if (a[i] == b[j]) {
+                commonElements.insert(a[i]);
+            }
+        }
+    }
+
+    cout << "Спільні елементи обох послідовностей: ";
+    for (const double& element : commonElements) {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
