7d0b0129cb3dbdfcb26e4ed839e4e96b568a075f
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-08 16:00:00 +0200

    lab4_3

---
+++ lab4_3/lab4_3.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream> 
+#include <cmath>
+
+
+
+using namespace std;
+
+
+
+int main()
+
+{
+    setlocale(LC_ALL, "Ukrainian");
+    double n; double x;
+
+
+
+    cout << "Введіть натуральне число n: ";
+
+    cin >> n;
+
+
+
+    cout << "Введіть число x: ";
+
+    cin >> x; int count = 0;
+
+
+
+    for (int i = 0; i < n; ++i)
+
+    {
+
+        double ai;
+
+
+
+        cout << "Введіть число a" << i + 1 << ": ";
+
+        cin >> ai;
+
+
+
+
+
+        if (sin(ai) == x)
+
+        {
+
+            count++;
+
+
+
+        }
+
+
+
+    }
+
+    cout << "Число " << x << " зустрічається серед sin(a1), sin(a2), .., sin(an) " << count << " разів." << endl;
+
+
+
+
+
+    return 0;
+
+}
