7d283011c48606f3da45cca2fe743ea571f086ae
xanax4rl <119081306+xanax4rl@users.noreply.github.com>
2023-11-09 18:01:56 +0200

    lab9_1

---
+++ lab9_1/lab9_1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int GCD(int A, int B) {
+    if (B == 0) {
+        return A;
+    }
+    else {
+        return GCD(B, A % B);
+    }
+}
+
+int main() {
+
+    setlocale(LC_ALL, "Ukrainian");
+
+    int A, B;
+
+    cout << "Введіть число A: ";
+    cin >> A;
+
+    cout << "Введіть число B: ";
+    cin >> B;
+
+    int result = GCD(A, B);
+
+    cout << "Найбільший спільний дільник (НСД) A і B: " << result << endl;
+
+    return 0;
+}
